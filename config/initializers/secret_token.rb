b7c7bae96e969e9c310ad7b599a571a04afd752b
Alexander <alexander.r.ambrose@gmail.com>
2011-08-13 12:43:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lordevents::Application.config.secret_token = '99513922e5599dedadce139b835456219f366968e1f5ce9269a85d198bbd73b4c1555bd7fad61149e7373d81c13a4180eda2230ac2748e6df7d175418c18a468'
